45d7ae279a95636aacf4a546c756b776da94347e
mudassir2626 <m.mudassir@directfn.com>
2023-04-04 16:50:38 +0500

    Commit All Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'admin',loadChildren:()=>import('./module/admin/admin-routing.module').then((m)=>m.AdminRoutingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { AdminDashboardComponent } from './module/components/admin-dashboard/admin-dashboard.component';
+import { HeaderComponent } from './module/components/header/header.component';
+import { MenuComponent } from './module/components/menu/menu.component';
+import { ContactusComponent } from './module/components/contactus/contactus.component';
+import { HomeComponent } from './module/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AdminDashboardComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContactusComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
